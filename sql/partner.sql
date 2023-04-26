9183cb6f2744ba21782833ef0ae390a10b57d2dc
帆 <1157712386@qq.com>
2023-04-26 20:46:47 +0800

    交友框架完结

--- sql/partner.sql
+++ sql/partner.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50738
  File Encoding         : 65001
 
- Date: 18/04/2023 17:01:34
+ Date: 26/04/2023 20:46:05
 */
 
 SET NAMES utf8mb4;
@@ -25,19 +25,186 @@ CREATE TABLE `dynamic`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '内容',
-  `imgs` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图片',
+  `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图片',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '简介',
   `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) DEFAULT NULL COMMENT '更新时间',
   `uid` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户标识',
-  `deleted` int(255) DEFAULT NULL COMMENT '删除标识',
+  `deleted` int(255) DEFAULT 0 COMMENT '删除标识',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '动态' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '动态' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of dynamic
 -- ----------------------------
-INSERT INTO `dynamic` VALUES (1, '测试', '测试内容', NULL, '测试', '2022-11-29 21:37:24', '2022-11-29 21:37:26', 'b80333662477430eab52d4c18acab2ad', NULL);
+INSERT INTO `dynamic` VALUES (1, '测试', '测试内容', 'http://localhost:9090/file/download/56ea6f90963143829e86f6b652d0b236.jpg', '测试', '2022-11-29 21:37:24', '2022-11-29 21:37:26', 'efb74e2cdc5b4ae680959d25b7cd101c', 0);
+INSERT INTO `dynamic` VALUES (2, '123', '123', 'http://localhost:9090/file/download/56ea6f90963143829e86f6b652d0b236.jpg', '123', '2023-04-26 16:31:08', '2023-04-26 16:31:08', 'efb74e2cdc5b4ae680959d25b7cd101c', 0);
+
+-- ----------------------------
+-- Table structure for im
+-- ----------------------------
+DROP TABLE IF EXISTS `im`;
+CREATE TABLE `im`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户编号',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户姓名',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '头像',
+  `sign` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '个性签名',
+  `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '消息文字',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '发送时间',
+  `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '图片',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im
+-- ----------------------------
+INSERT INTO `im` VALUES (1, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '1', '2023-04-25 20:38:46', NULL);
+INSERT INTO `im` VALUES (2, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '1', '2023-04-25 20:38:55', NULL);
+INSERT INTO `im` VALUES (3, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, 'a', '2023-04-25 20:45:44', NULL);
+INSERT INTO `im` VALUES (4, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, 'a', '2023-04-25 20:52:02', NULL);
+INSERT INTO `im` VALUES (5, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '1', '2023-04-25 20:58:57', NULL);
+INSERT INTO `im` VALUES (6, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '1\n', '2023-04-25 21:11:23', NULL);
+INSERT INTO `im` VALUES (7, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '😃', '2023-04-25 21:11:46', NULL);
+INSERT INTO `im` VALUES (8, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, 'a', '2023-04-25 21:12:02', NULL);
+INSERT INTO `im` VALUES (9, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, 'a', '2023-04-25 21:12:32', NULL);
+INSERT INTO `im` VALUES (10, '5a4f2a4963c148d1a27cb8bdb712792c', '管理员', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, 'sasa', '2023-04-25 21:15:56', NULL);
+INSERT INTO `im` VALUES (11, 'b4f3422e6c82439ea3e112d69091584e', 'abb', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '22', '2023-04-25 21:30:36', NULL);
+INSERT INTO `im` VALUES (12, 'b4f3422e6c82439ea3e112d69091584e', 'abb', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', NULL, '😉😇🥰😋🤨', '2023-04-25 21:35:14', NULL);
+
+-- ----------------------------
+-- Table structure for sys_dict
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dict`;
+CREATE TABLE `sys_dict`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '编码',
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '内容',
+  `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '类型',
+  `deleted` int(11) DEFAULT 0 COMMENT '删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `c_d`(`code`, `deleted`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_dict
+-- ----------------------------
+INSERT INTO `sys_dict` VALUES (1, 'user', 'user', 'icon', 0);
+INSERT INTO `sys_dict` VALUES (2, 'message', 'message', 'icon', 0);
+INSERT INTO `sys_dict` VALUES (3, 'menu', 'menu', 'icon', 0);
+INSERT INTO `sys_dict` VALUES (4, 'grid', 'grid', 'icon', 0);
+INSERT INTO `sys_dict` VALUES (5, 'star', 'star', 'icon', 0);
+INSERT INTO `sys_dict` VALUES (6, 'house', 'house', 'icon', 0);
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '名称',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '路径',
+  `orders` int(11) DEFAULT 1 COMMENT '顺序',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'grid' COMMENT '图标',
+  `page` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '页面路径',
+  `auth` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '权限',
+  `pid` int(11) DEFAULT NULL COMMENT '父级id',
+  `deleted` int(1) DEFAULT 0 COMMENT '逻辑删除',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) DEFAULT NULL COMMENT '更新时间',
+  `type` int(1) DEFAULT NULL COMMENT '类型，1目录  2菜单 3按钮',
+  `hide` tinyint(1) DEFAULT 0 COMMENT '是否隐藏',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `a_d_index`(`auth`, `deleted`) USING BTREE,
+  UNIQUE INDEX `p_p_d_index`(`path`, `page`, `deleted`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES (1, '系统管理', '', 2, 'menu', NULL, NULL, NULL, 0, '2023-01-16 20:45:51', '2023-01-16 20:45:51', 1, 0);
+INSERT INTO `sys_permission` VALUES (3, '用户管理', 'user', 1, 'grid', 'User', 'user.list', 1, 0, '2023-01-16 20:45:51', '2023-01-16 20:45:51', 2, 0);
+INSERT INTO `sys_permission` VALUES (4, '用户新增', '', 1, 'grid', '', 'user.add', 3, 0, '2023-01-16 20:45:51', '2023-01-16 20:45:51', 3, 0);
+INSERT INTO `sys_permission` VALUES (8, '用户编辑', '', 1, 'grid', NULL, 'user.edit', 3, 0, NULL, '2023-01-28 11:45:21', 3, 0);
+INSERT INTO `sys_permission` VALUES (9, '用户删除', NULL, 1, 'grid', NULL, 'user.delete', 3, 0, '2023-01-29 11:04:15', '2023-01-29 11:04:15', 3, 0);
+INSERT INTO `sys_permission` VALUES (10, '角色管理', 'role', 1, 'grid', 'Role', 'role', 1, 0, '2023-04-23 10:30:59', NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (11, '权限管理', 'permission', 1, 'grid', 'Permission', 'permission.list', 1, 0, '2023-04-23 10:31:37', NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (12, '首页', 'home', 1, 'house', 'Home', '', NULL, 0, '2023-04-23 10:49:36', NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (13, '数据字典管理', 'dict', 1, 'grid', 'Dict', 'dict.list', 1, 0, '2023-04-23 20:43:01', NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (14, '批量删除', NULL, 1, 'grid', NULL, 'user.deleteBatch', 3, 0, '2023-04-24 12:34:13', NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (15, '用户导入', NULL, 1, 'grid', NULL, 'user.import', 3, 0, '2023-04-24 12:34:49', NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (16, '用户导出', NULL, 1, 'grid', NULL, 'user.export', 3, 0, '2023-04-24 12:35:03', NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (17, '聊天管理', 'im', 1, 'grid', 'Im', 'im.list', NULL, 0, '2023-04-25 20:09:08', NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (18, '动态管理', 'dynamic', 1, 'grid', 'Dynamic', NULL, NULL, 0, NULL, NULL, 2, 0);
+INSERT INTO `sys_permission` VALUES (19, '动态查询', NULL, 1, 'grid', NULL, 'dynamic.list', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (20, '动态新增', NULL, 1, 'grid', NULL, 'dynamic.add', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (21, '动态导入', NULL, 1, 'grid', NULL, 'dynamic.import', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (22, '动态导出', NULL, 1, 'grid', NULL, 'dynamic.export', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (23, '批量删除', NULL, 1, 'grid', NULL, 'dynamic.deleteBatch', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (24, '动态编辑', NULL, 1, 'grid', NULL, 'dynamic.edit', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (25, '动态删除', NULL, 1, 'grid', NULL, 'dynamic.delete', 18, 0, NULL, NULL, 3, 0);
+INSERT INTO `sys_permission` VALUES (26, '热门动态', NULL, 1, 'grid', NULL, 'dynamic.list.hot', 18, 0, '2023-04-26 17:24:59', NULL, 3, 0);
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '名称',
+  `flag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '唯一标识',
+  `deleted` int(1) DEFAULT 0 COMMENT '逻辑删除',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `flag_deleted_index`(`flag`, `deleted`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (2, '管理员', 'ADMIN', 0, '2023-04-19 22:07:50', NULL);
+INSERT INTO `sys_role` VALUES (3, '普通用户', 'USER', 0, '2023-04-19 22:08:15', NULL);
+
+-- ----------------------------
+-- Table structure for sys_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_permission`;
+CREATE TABLE `sys_role_permission`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `role_id`(`role_id`, `permission_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 506 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_permission
+-- ----------------------------
+INSERT INTO `sys_role_permission` VALUES (492, 2, 1);
+INSERT INTO `sys_role_permission` VALUES (493, 2, 3);
+INSERT INTO `sys_role_permission` VALUES (494, 2, 4);
+INSERT INTO `sys_role_permission` VALUES (495, 2, 8);
+INSERT INTO `sys_role_permission` VALUES (496, 2, 9);
+INSERT INTO `sys_role_permission` VALUES (500, 2, 10);
+INSERT INTO `sys_role_permission` VALUES (501, 2, 11);
+INSERT INTO `sys_role_permission` VALUES (482, 2, 12);
+INSERT INTO `sys_role_permission` VALUES (502, 2, 13);
+INSERT INTO `sys_role_permission` VALUES (497, 2, 14);
+INSERT INTO `sys_role_permission` VALUES (498, 2, 15);
+INSERT INTO `sys_role_permission` VALUES (499, 2, 16);
+INSERT INTO `sys_role_permission` VALUES (483, 2, 17);
+INSERT INTO `sys_role_permission` VALUES (484, 2, 18);
+INSERT INTO `sys_role_permission` VALUES (485, 2, 19);
+INSERT INTO `sys_role_permission` VALUES (486, 2, 20);
+INSERT INTO `sys_role_permission` VALUES (487, 2, 21);
+INSERT INTO `sys_role_permission` VALUES (488, 2, 22);
+INSERT INTO `sys_role_permission` VALUES (489, 2, 23);
+INSERT INTO `sys_role_permission` VALUES (490, 2, 24);
+INSERT INTO `sys_role_permission` VALUES (491, 2, 25);
+INSERT INTO `sys_role_permission` VALUES (504, 3, 12);
+INSERT INTO `sys_role_permission` VALUES (503, 3, 18);
+INSERT INTO `sys_role_permission` VALUES (505, 3, 26);
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -55,20 +222,24 @@ CREATE TABLE `sys_user`  (
   `create_time` datetime(0) DEFAULT NULL,
   `update_time` datetime(0) DEFAULT NULL,
   `avatar` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '头像',
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ADMIN' COMMENT '角色',
+  `sign` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '个性签名',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uid_index`(`uid`) USING BTREE,
   UNIQUE INDEX `username_index`(`username`, `deleted`) USING BTREE,
   UNIQUE INDEX `email_index`(`email`, `deleted`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 51 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 53 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'bgg', '$2a$10$KYJgiVBuZIWmZni93GBmC.g2fMTVhja8tzZ1LtToGOf44q1E2Z8Cq', 'Me', '河北保定', '1157712386@qq.com', 'efb74e2cdc5b4ae680959d25b7cd101c', 0, '2023-03-01 16:06:50', '2023-03-01 16:06:50', 'loginId=5a4f2a4963c148d1a27cb8bdb712792c&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJsb2dpbiIsImxvZ2luSWQiOiI1YTRmMmE0OTYzYzE0OGQxYTI3Y2I4YmRiNzEyNzkyYyIsInJuU3RyIjoiNDJtdGFyUjE0TkVnUjhqTXFrYmdoNHFXZkJEWjgwc24ifQ.Hr6CIGaeG42nQmZZp8DQlDJEqyxK_ICleM7c19xYhR4');
-INSERT INTO `sys_user` VALUES (2, 'admin', '$2a$10$N6Yd/ifpjDgvw8yJ3x2hOOM/qIU6RoM2kmSlzRKfdS2fY5gQgz.E2', '管理员', '河北保定', '1516545@qq.com', '5a4f2a4963c148d1a27cb8bdb712792c', 0, '2023-04-09 10:57:12', '2023-04-09 10:57:12', 'http://localhost:9090/file/download/11b69004e05948dcbd1f00e929e2698e.jpg?loginId=5a4f2a4963c148d1a27cb8bdb712792c&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJsb2dpbiIsImxvZ2luSWQiOiI1YTRmMmE0OTYzYzE0OGQxYTI3Y2I4YmRiNzEyNzkyYyIsInJuU3RyIjoiNDJtdGFyUjE0TkVnUjhqTXFrYmdoNHFXZkJEWjgwc24ifQ.Hr6CIGaeG42nQmZZp8DQlDJEqyxK_ICleM7c19xYhR4');
-INSERT INTO `sys_user` VALUES (47, 'bwwwgg', '$2a$10$QQ2DkXQDxWtbvtyV2G9fiufMppqs8AyWm5bDjd7F9ROUXLIpeqCeu', '系统用户od1cgg', '河北保定', '11577386@qq.com', 'f01a9f5518b94aa6ba6923893be08ba9', 0, '2023-04-18 12:26:31', '2023-04-18 12:26:31', '');
-INSERT INTO `sys_user` VALUES (48, 'qqqqqqqq', '$2a$10$vWA4k9wDZe8Xwpii2UF0Oe95WIyuQmwgggF6E0Mnsc5TBVaVU.3de', '系统用户23u3al', '河北保定', '15112316545@qq.com', '2ad8ecc27d87420a8ff33f4bf7c168c4', 48, '2023-04-18 12:26:31', '2023-04-18 12:26:31', '');
-INSERT INTO `sys_user` VALUES (49, 'agasg', '$2a$10$vxcyJMn1xuvaeWsddr.yf.9D41ZD7Lvdv/zhVAls21i.egcIZ1Zz2', '系统用户hrslea', '河北保定', '13123123@qq.com', '14c09e7a307f4620a721776256681049', 49, '2023-04-18 12:27:06', '2023-04-18 12:27:06', '');
-INSERT INTO `sys_user` VALUES (50, 'agaag', '$2a$10$5yJPrEAZK3i3Nly1.s0wj.UZC/XtnsjWl4w9OPvYrEIZK5xxkjh1i', '系统用户apj0p7', '河北唐山', '1221321312@qq.com', '8c1790c20ab746dfae9a4044d68e2a61', 50, '2023-04-18 12:27:06', '2023-04-18 12:27:06', '');
+INSERT INTO `sys_user` VALUES (1, 'bgg', '$2a$10$KYJgiVBuZIWmZni93GBmC.g2fMTVhja8tzZ1LtToGOf44q1E2Z8Cq', 'Me', '河北保定', '115771236@qq.com', 'efb74e2cdc5b4ae680959d25b7cd101c', 0, '2023-03-01 16:06:50', '2023-03-01 16:06:50', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'USER', '1');
+INSERT INTO `sys_user` VALUES (2, 'admin', '$2a$10$N6Yd/ifpjDgvw8yJ3x2hOOM/qIU6RoM2kmSlzRKfdS2fY5gQgz.E2', '管理员', '河北保定', '151622545@qq.com', '5a4f2a4963c148d1a27cb8bdb712792c', 0, '2023-04-09 10:57:12', '2023-04-09 10:57:12', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (47, 'bwwwg', '$2a$10$QQ2DkXQDxWtbvtyV2G9fiufMppqs8AyWm5bDjd7F9ROUXLIpeqCeu', '系统用户od1cgg', '河北保定', '11577386@qq.com', 'f01a9f5518b94aa6ba6923893be08ba9', 0, '2023-04-18 12:26:31', '2023-04-18 12:26:31', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (48, 'qqqqqqqq', '$2a$10$vWA4k9wDZe8Xwpii2UF0Oe95WIyuQmwgggF6E0Mnsc5TBVaVU.3de', '系统用户23u3al', '河北保定', '15112316545@qq.com', '2ad8ecc27d87420a8ff33f4bf7c168c4', 48, '2023-04-18 12:26:31', '2023-04-18 12:26:31', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (49, 'agasg', '$2a$10$vxcyJMn1xuvaeWsddr.yf.9D41ZD7Lvdv/zhVAls21i.egcIZ1Zz2', '系统用户hrslea', '河北保定', '13123123@qq.com', '14c09e7a307f4620a721776256681049', 49, '2023-04-18 12:27:06', '2023-04-18 12:27:06', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (50, 'agaag', '$2a$10$5yJPrEAZK3i3Nly1.s0wj.UZC/XtnsjWl4w9OPvYrEIZK5xxkjh1i', '系统用户apj0p7', '河北唐山', '1221321312@qq.com', '8c1790c20ab746dfae9a4044d68e2a61', 50, '2023-04-18 12:27:06', '2023-04-18 12:27:06', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (51, 'agg', '$2a$10$j69nAm8jRNlYrbwycJXIBecpXMkREEUtT3MzNhR2tstyv0JF6AWyi', '系统用户bc8rxq', '河北邯郸', '115771@qq.com', '3369f5e084574870bd6b109639c31f15', 0, '2023-04-22 08:34:50', '2023-04-22 08:34:50', 'http://localhost:9090/file/download/3b0159d59eab4edbb07dcc21776acf99.jpg', 'ADMIN', '1');
+INSERT INTO `sys_user` VALUES (52, 'abb', '$2a$10$SnsG9m8zAFTqqYz9A00HSOmsnBMxGqChgcD.IWt4Hmlle89jI9rSm', 'abb', '河北邯郸', '12312312@166.com', 'b4f3422e6c82439ea3e112d69091584e', 0, '2023-04-25 20:24:07', '2023-04-25 20:24:07', 'http://localhost:9090/file/download/846c0d3b444845189ef1fd6643ec8f24.jpg', 'ADMIN', '1');
 
 SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/com/partner/boot/controller/DynamicController.java
+++ src/main/java/com/partner/boot/controller/DynamicController.java
@@ -1,122 +1,160 @@
 package com.partner.boot.controller;
 
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelWriter;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletOutputStream;
 import java.net.URLEncoder;
+
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.partner.boot.common.Constants;
+import com.partner.boot.entity.User;
+import com.partner.boot.service.IUserService;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 import java.io.InputStream;
 import java.util.List;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.partner.boot.common.Result;
 import org.springframework.web.multipart.MultipartFile;
 import com.partner.boot.service.IDynamicService;
 import com.partner.boot.entity.Dynamic;
 
-    import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
-* <p>
-    * 动态 前端控制器
-    * </p>
-*
-* @author 现计科1901武泊帆
-* @since 2023-04-19
-*/
-    @RestController
+ * <p>
+ * 动态 前端控制器
+ * </p>
+ *
+ * @author 计科1901武泊帆
+ * @since 2023-04-25
+ */
+@RestController
 @RequestMapping("/dynamic")
-        public class DynamicController {
+public class DynamicController {
 
     @Resource
     private IDynamicService dynamicService;
 
-    // 新增
+    @Resource
+    IUserService userService;
+
     @PostMapping
-    public Result save(@RequestBody Dynamic dynamic) {
-            dynamicService.save(dynamic);
-            return Result.success();
+    @SaCheckPermission("dynamic.add")
+    public Result save(@RequestBody Dynamic dynamic) {//在后台获取用户信息
+        User user = (User) StpUtil.getSession().get(Constants.LOGIN_USER_KEY);
+        dynamic.setUid(user.getUid());
+        dynamicService.save(dynamic);
+        return Result.success();
     }
-    //更新
+
     @PutMapping
+    @SaCheckPermission("dynamic.edit")
     public Result update(@RequestBody Dynamic dynamic) {
-            dynamicService.updateById(dynamic);
-            return Result.success();
+        dynamicService.updateById(dynamic);
+        return Result.success();
     }
 
     @DeleteMapping("/{id}")
+    @SaCheckPermission("dynamic.delete")
     public Result delete(@PathVariable Integer id) {
-    dynamicService.removeById(id);
-    return Result.success();
+        dynamicService.removeById(id);
+        return Result.success();
     }
 
     @PostMapping("/del/batch")
+    @SaCheckPermission("dynamic.deleteBatch")
     public Result deleteBatch(@RequestBody List<Integer> ids) {
-    dynamicService.removeByIds(ids);
-    return Result.success();
+        dynamicService.removeByIds(ids);
+        return Result.success();
     }
 
     @GetMapping
+    @SaCheckPermission("dynamic.list")
     public Result findAll() {
-    return Result.success(dynamicService.list());
+        return Result.success(dynamicService.list());
+    }
+
+    @GetMapping("/hot")
+    @SaCheckPermission("dynamic.list.hot")
+    public Result hot(@RequestParam Integer pageNum,
+                      @RequestParam Integer pageSize) {
+        QueryWrapper<Dynamic> queryWrapper = new QueryWrapper<Dynamic>().orderByDesc("id");
+        Page<Dynamic> page = dynamicService.page(new Page<>(pageNum, pageSize), queryWrapper);
+        List<User> users = userService.list();
+        for (Dynamic record : page.getRecords()) {
+            //ifPresent表示数据筛选有结果的时候触发
+            users.stream().filter(user -> user.getUid().equals(record.getUid())).findFirst().ifPresent(record::setUser);
+
+        }
+        return Result.success(page);
     }
 
     @GetMapping("/{id}")
+    @SaCheckPermission("dynamic.list")
     public Result findOne(@PathVariable Integer id) {
-    return Result.success(dynamicService.getById(id));
+        return Result.success(dynamicService.getById(id));
     }
 
     @GetMapping("/page")
+    @SaCheckPermission("dynamic.list")
     public Result findPage(@RequestParam(defaultValue = "") String name,
-    @RequestParam Integer pageNum,
-    @RequestParam Integer pageSize) {
-    QueryWrapper<Dynamic> queryWrapper = new QueryWrapper<Dynamic>().orderByDesc("id");
-    queryWrapper.like(!"".equals(name), "name", name);
-    return Result.success(dynamicService.page(new Page<>(pageNum, pageSize), queryWrapper));
+                           @RequestParam Integer pageNum,
+                           @RequestParam Integer pageSize) {
+        QueryWrapper<Dynamic> queryWrapper = new QueryWrapper<Dynamic>().orderByDesc("id");
+        queryWrapper.like(!"".equals(name), "name", name);
+        return Result.success(dynamicService.page(new Page<>(pageNum, pageSize), queryWrapper));
     }
 
     /**
-    * 导出接口
-    */
+     * 导出接口
+     */
     @GetMapping("/export")
+    @SaCheckPermission("dynamic.export")
     public void export(HttpServletResponse response) throws Exception {
-    // 从数据库查询出所有的数据
-    List<Dynamic> list = dynamicService.list();
-    // 在内存操作，写出到浏览器
-    ExcelWriter writer = ExcelUtil.getWriter(true);
+        // 从数据库查询出所有的数据
+        List<Dynamic> list = dynamicService.list();
+        // 在内存操作，写出到浏览器
+        ExcelWriter writer = ExcelUtil.getWriter(true);
 
-    // 一次性写出list内的对象到excel，使用默认样式，强制输出标题
-    writer.write(list, true);
+        // 一次性写出list内的对象到excel，使用默认样式，强制输出标题
+        writer.write(list, true);
 
-    // 设置浏览器响应的格式
-    response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
-    String fileName = URLEncoder.encode("Dynamic信息表", "UTF-8");
-    response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+        // 设置浏览器响应的格式
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("Dynamic信息表", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
 
-    ServletOutputStream out = response.getOutputStream();
-    writer.flush(out, true);
-    out.close();
-    writer.close();
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
 
     }
 
     /**
-    * excel 导入
-    * @param file
-    * @throws Exception
-    */
+     * excel 导入
+     *
+     * @param file
+     * @throws Exception
+     */
     @PostMapping("/import")
+    @SaCheckPermission("dynamic.import")
     public Result imp(MultipartFile file) throws Exception {
-    InputStream inputStream = file.getInputStream();
-    ExcelReader reader = ExcelUtil.getReader(inputStream);
-    // 通过 javabean的方式读取Excel内的对象，但是要求表头必须是英文，跟javabean的属性要对应起来
-    List<Dynamic> list = reader.readAll(Dynamic.class);
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        // 通过 javabean的方式读取Excel内的对象，但是要求表头必须是英文，跟javabean的属性要对应起来
+        List<Dynamic> list = reader.readAll(Dynamic.class);
 
-    dynamicService.saveBatch(list);
-    return Result.success();
+        dynamicService.saveBatch(list);
+        return Result.success();
     }
 
-    }
+}
